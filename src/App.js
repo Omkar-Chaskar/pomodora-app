95f3fbc995bc53ff8ed36e93d139fe124e2a6679
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2022-06-09 19:11:15 +0530

    refactor : Remove unwanted page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./component/header";
 import Navigation from "./component/navigation";
 import Home from "./component/home";
 import Archive from "./component/archive";
-import Label from "./component/label";
 import Trash from "./component/trash";
 import AddNote from "./component/addnote";
 import Pomodora from "./component/pomodora";
@@ -16,7 +15,6 @@ export default function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Label" element={<Label />} />
         <Route path="Archive" element={<Archive />} />
         <Route path="Trash" element={<Trash />} />
         <Route path="AddNote" element={<AddNote />} />
